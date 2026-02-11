0703c4939bd9eeb9f971e181a7f0fe4c6e0753db
Allana Nicole Picana <anpicana@hawaii.edu>
2026-02-11 08:06:27 +0000

    updated code

--- week5/f2c/index.js
+++ week5/f2c/index.js
@@ -7,47 +7,43 @@ const bodyParser = require("body-parser");
 // create an app object from the express object
 const app = express();
 // this allows the parsing of the html file using body parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //this sends the html file to the web page using the root directory
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/f2cCalc.html"); //changed to correct file name
 });
 
 // this gets the response from the values in the web page
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
+  var fTemp = Number(req.temp.n1);
 
-  var num1 = Number(req.body.n1);
-  var num2 = Number(req.body.n2);
+  // does the computation of the input variables, as numbers
+  var result = fTemp;
 
-// does the computation of the input variables, as numbers
-  var result = num1 * num2;
-
-// sends the results back to the web page as string
-  res.send("The result of the calculation is " + result);
-})
+  // sends the results back to the web page as string
+  res.send("The result of the calculation is " + result + " C");
+});
 //this gets the response from the web page to this placeholder
 
-// this code is only invoked on the path /bmiCalculator
-app.get("/bmiCalculator", function(req, res){
-  res.sendFile(__dirname + "/bmiCalculator.html");
+// this code is only invoked on the path /f2cCalculator
+app.get("/f2cCalculator", function (req, res) {
+  res.sendFile(__dirname + "/f2cCalc.html");
 });
 
 // invoked on the submit button
-app.post("/bmiCalculator", function(req, res){
+app.post("/f2cCalculator", function (req, res) {
+  //converts the string input to a float number
+  var fTemp = parseFloat(req.temp.n1);
+  
 
-//converts the string input to a float number
-  var weight = parseFloat(req.body.weight);
-  var height = parseFloat(req.body.height);
+  // does the computation of the input variables, as numbers
+  var f2c = (fTemp) - 32) x 5/9;
 
-// does the computation of the input variables, as numbers
-  var bmi = weight / (height * weight);
+  //display the result in 2 decimal places
+  res.send(fTemp + "Fahrenheit converted to Centigrade is " + f2c.toFixed(2));
+});
 
-//display the result in 2 decimal places
-res.send("Your BMI is " + bmi.toFixed(2));
-
-})
-
-app.listen(3000, function() {
-  console.log ("Server is running on port 3000")
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
